fda21e6a912af1c18ae940c9dc8c1876e90f743a
prashantrajput11 <prashanttanwar.work@example.com>
2024-09-30 00:06:26 +0530

    intermaite questions done

--- practice/es6/intermediate/ques1.js
+++ practice/es6/intermediate/ques1.js
@@ -1,10 +1,35 @@
 // Problem: Extracting Social Media Post Data (Array Destructuring, Object Destructuring)
 // 	•	Scenario: You’re working on a social media platform and have an array of posts. You need to extract the first post’s details (like title and content) to show as a featured post.
 
-// const posts = [
-//   { id: 101, title: 'Learning JavaScript', content: 'JavaScript is awesome!', likes: 150 },
-//   { id: 102, title: 'React Tips', content: 'Use hooks for better performance!', likes: 200 },
-//   { id: 103, title: 'Node.js Basics', content: 'Understand event-driven architecture.', likes: 100 }
-// ];
+const posts = [
+	{
+		id: 101,
+		title: "Learning JavaScript",
+		content: "JavaScript is awesome!",
+		likes: 150,
+	},
+	{
+		id: 102,
+		title: "React Tips",
+		content: "Use hooks for better performance!",
+		likes: 200,
+	},
+	{
+		id: 103,
+		title: "Node.js Basics",
+		content: "Understand event-driven architecture.",
+		likes: 100,
+	},
+];
 
 // Task: Use array and object destructuring to extract the first post’s title and content into separate variables.
+
+// const [firstPost] = posts;
+
+// const { title, content } = firstPost;
+
+// shorthand
+
+const [{ title, content }] = posts;
+
+console.log(title, content);
--- practice/es6/intermediate/ques2.js
+++ practice/es6/intermediate/ques2.js
@@ -2,10 +2,17 @@
 // 	•	Scenario: In an ecommerce platform, the price of certain products needs to be updated, but the original price data should remain unchanged for record-keeping.
 // 	•	Dummy Data:
 
-// const products = [
-// 	{ id: 1, name: "Laptop", price: 1200 },
-// 	{ id: 2, name: "Smartphone", price: 700 },
-// 	{ id: 3, name: "Tablet", price: 500 },
-// ];
+const products = [
+	{ id: 1, name: "Laptop", price: 1200 },
+	{ id: 2, name: "Smartphone", price: 700 },
+	{ id: 3, name: "Tablet", price: 500 },
+];
 
 // Create a new array where you update the price of the laptop to 1300, without modifying the original products array.
+
+// const newItems = [...products, products[0] && { products[0].price: 1300 }]; ❌
+
+const newItems = products.map((product) =>
+	product.id === 1 ? { ...product, price: 1300 } : product
+);
+console.log(newItems);
